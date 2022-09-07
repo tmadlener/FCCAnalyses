9c087640d7a64b547575e550718d6eb55bbcf265
Birgit Stapf <bstapf@nikhef.nl>
2022-09-07 11:49:26 +0200

    add jet variables - bjets with 3 different WP

--- run/Delphescard_validation/analysis.py
+++ run/Delphescard_validation/analysis.py
@@ -67,14 +67,14 @@ class analysis():
 			.Define("eta_jets",  "FCCAnalyses::ReconstructedParticle::get_eta(Jet)")
 
 			# #selected jets above a pT threshold of 20 GeV
-			# .Define("selected_jets", "FCCAnalyses::ReconstructedParticle::sel_pt(20.)(Jet)") 
-			# .Define("n_jets_sel",  "FCCAnalyses:ReconstructedParticle::get_n(selected_jets)")
-			# .Define("px_jets_sel",  "FCCAnalyses:ReconstructedParticle::get_px(selected_jets)")
-			# .Define("py_jets_sel",  "FCCAnalyses:ReconstructedParticle::get_py(selected_jets)")
-			# .Define("pz_jets_sel",  "FCCAnalyses:ReconstructedParticle::get_pz(selected_jets)")
-			# .Define("E_jets_sel",  "FCCAnalyses:ReconstructedParticle::get_e(selected_jets)")
-			# .Define("pT_jets_sel",  "FCCAnalyses:ReconstructedParticle::get_pt(selected_jets)")
-			# .Define("eta_jets_sel",  "FCCAnalyses:ReconstructedParticle::get_eta(selected_jets)")
+			.Define("selected_jets", "FCCAnalyses::ReconstructedParticle::sel_pt(20.)(Jet)") 
+			.Define("n_jets_sel",  "FCCAnalyses::ReconstructedParticle::get_n(selected_jets)")
+			.Define("px_jets_sel",  "FCCAnalyses::ReconstructedParticle::get_px(selected_jets)")
+			.Define("py_jets_sel",  "FCCAnalyses::ReconstructedParticle::get_py(selected_jets)")
+			.Define("pz_jets_sel",  "FCCAnalyses::ReconstructedParticle::get_pz(selected_jets)")
+			.Define("E_jets_sel",  "FCCAnalyses::ReconstructedParticle::get_e(selected_jets)")
+			.Define("pT_jets_sel",  "FCCAnalyses::ReconstructedParticle::get_pt(selected_jets)")
+			.Define("eta_jets_sel",  "FCCAnalyses::ReconstructedParticle::get_eta(selected_jets)")
 
 			# #b-tagged jets:
 			.Alias("Jet3","Jet#3.index") 
@@ -82,14 +82,32 @@ class analysis():
 			#LOOSE WP
 			.Define("b_tagged_jets_loose", "AnalysisFCChh::get_tagged_jets(Jet, Jet3, ParticleIDs, ParticleIDs_0, 0)") #bit 0 = loose WP, see: https://github.com/delphes/delphes/blob/master/cards/FCC/scenarios/FCChh_I.tcl
 			.Define("n_b_jets_loose", "FCCAnalyses::ReconstructedParticle::get_n(b_tagged_jets_loose)")
+			.Define("px_b_jets_loose",  "FCCAnalyses::ReconstructedParticle::get_px(b_tagged_jets_loose)")
+			.Define("py_b_jets_loose",  "FCCAnalyses::ReconstructedParticle::get_py(b_tagged_jets_loose)")
+			.Define("pz_b_jets_loose",  "FCCAnalyses::ReconstructedParticle::get_pz(b_tagged_jets_loose)")
+			.Define("E_b_jets_loose",  "FCCAnalyses::ReconstructedParticle::get_e(b_tagged_jets_loose)")
+			.Define("pT_b_jets_loose",  "FCCAnalyses::ReconstructedParticle::get_pt(b_tagged_jets_loose)")
+			.Define("eta_b_jets_loose",  "FCCAnalyses::ReconstructedParticle::get_eta(b_tagged_jets_loose)")
 
 			#MEDIUM WP
 			.Define("b_tagged_jets_medium", "AnalysisFCChh::get_tagged_jets(Jet, Jet3, ParticleIDs, ParticleIDs_0, 1)") #bit 1 = medium WP, see: https://github.com/delphes/delphes/blob/master/cards/FCC/scenarios/FCChh_I.tcl
 			.Define("n_b_jets_medium", "FCCAnalyses::ReconstructedParticle::get_n(b_tagged_jets_medium)")
+			.Define("px_b_jets_medium",  "FCCAnalyses::ReconstructedParticle::get_px(b_tagged_jets_medium)")
+			.Define("py_b_jets_medium",  "FCCAnalyses::ReconstructedParticle::get_py(b_tagged_jets_medium)")
+			.Define("pz_b_jets_medium",  "FCCAnalyses::ReconstructedParticle::get_pz(b_tagged_jets_medium)")
+			.Define("E_b_jets_medium",  "FCCAnalyses::ReconstructedParticle::get_e(b_tagged_jets_medium)")
+			.Define("pT_b_jets_medium",  "FCCAnalyses::ReconstructedParticle::get_pt(b_tagged_jets_medium)")
+			.Define("eta_b_jets_medium",  "FCCAnalyses::ReconstructedParticle::get_eta(b_tagged_jets_medium)")
 
 			#TIGHT WP
 			.Define("b_tagged_jets_tight", "AnalysisFCChh::get_tagged_jets(Jet, Jet3, ParticleIDs, ParticleIDs_0, 2)") #bit 2 = tight WP, see: https://github.com/delphes/delphes/blob/master/cards/FCC/scenarios/FCChh_I.tcl
 			.Define("n_b_jets_tight", "FCCAnalyses::ReconstructedParticle::get_n(b_tagged_jets_tight)")
+			.Define("px_b_jets_tight",  "FCCAnalyses::ReconstructedParticle::get_px(b_tagged_jets_tight)")
+			.Define("py_b_jets_tight",  "FCCAnalyses::ReconstructedParticle::get_py(b_tagged_jets_tight)")
+			.Define("pz_b_jets_tight",  "FCCAnalyses::ReconstructedParticle::get_pz(b_tagged_jets_tight)")
+			.Define("E_b_jets_tight",  "FCCAnalyses::ReconstructedParticle::get_e(b_tagged_jets_tight)")
+			.Define("pT_b_jets_tight",  "FCCAnalyses::ReconstructedParticle::get_pt(b_tagged_jets_tight)")
+			.Define("eta_b_jets_tight",  "FCCAnalyses::ReconstructedParticle::get_eta(b_tagged_jets_tight)")
 
 
 
@@ -102,7 +120,11 @@ class analysis():
 		for branchName in [
 			#Jets:
 			"n_jets", "px_jets", "py_jets", "pz_jets", "E_jets", "pT_jets", "eta_jets",
-			'n_b_jets_loose', 'n_b_jets_medium', 'n_b_jets_tight',
+			"n_jets_sel", "px_jets_sel", "py_jets_sel", "pz_jets_sel", "E_jets_sel", "pT_jets_sel", "eta_jets_sel",
+			"n_b_jets_loose", "px_b_jets_loose", "py_b_jets_loose", "pz_b_jets_loose", "E_b_jets_loose", "pT_b_jets_loose", "eta_b_jets_loose",
+			"n_b_jets_medium", "px_b_jets_medium", "py_b_jets_medium", "pz_b_jets_medium", "E_b_jets_medium", "pT_b_jets_medium", "eta_b_jets_medium",
+			"n_b_jets_tight", "px_b_jets_tight", "py_b_jets_tight", "pz_b_jets_tight", "E_b_jets_tight", "pT_b_jets_tight", "eta_b_jets_tight",
+
 			]:
 			branchList.push_back(branchName)
 
