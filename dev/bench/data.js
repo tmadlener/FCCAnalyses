b19316d611b638fdb406ccdec278e3df2ac51f1d
github-action-benchmark <github@users.noreply.github.com>
2022-11-30 15:21:36 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for 94885e852d9a3c7ba86fcb78eb113dcff47d0185

---
+++ dev/bench/data.js
@@ -0,0 +1,60 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1669821696703,
+  "repoUrl": "https://github.com/tmadlener/FCCAnalyses",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "thomas.madlener@desy.de",
+            "name": "Thomas Madlener",
+            "username": "tmadlener"
+          },
+          "committer": {
+            "email": "thomas.madlener@desy.de",
+            "name": "Thomas Madlener",
+            "username": "tmadlener"
+          },
+          "distinct": true,
+          "id": "94885e852d9a3c7ba86fcb78eb113dcff47d0185",
+          "message": "Install the FastJet addons headers to work around #227",
+          "timestamp": "2022-11-30T16:13:21+01:00",
+          "tree_id": "ba14dca91bd76b33b9a8ecd5bf93b3ede9c08f53",
+          "url": "https://github.com/tmadlener/FCCAnalyses/commit/94885e852d9a3c7ba86fcb78eb113dcff47d0185"
+        },
+        "date": 1669821695878,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "nightly | Time spent running the analysis: case-studies/analysis_example/scripts",
+            "value": 10.027430295944214,
+            "unit": "Seconds",
+            "range": 10,
+            "extra": "Analysis path: case-studies/analysis_example/scripts"
+          },
+          {
+            "name": "nightly | Time spent running the analysis: examples/FCCee/higgs/mH-recoil/mumu",
+            "value": 11.37247109413147,
+            "unit": "Seconds",
+            "range": 10,
+            "extra": "Analysis path: examples/FCCee/higgs/mH-recoil/mumu"
+          },
+          {
+            "name": "nightly | Time spent running the analysis: examples/FCCee/flavour/Bc2TauNu",
+            "value": 15.39921498298645,
+            "unit": "Seconds",
+            "range": 10,
+            "extra": "Analysis path: examples/FCCee/flavour/Bc2TauNu"
+          },
+          {
+            "name": "nightly | Time spent running the analysis: examples/FCCee/test",
+            "value": 12.518760681152344,
+            "unit": "Seconds",
+            "range": 10,
+            "extra": "Analysis path: examples/FCCee/test"
+          }
+        ]
+      }
+    ]
+  }
+}
